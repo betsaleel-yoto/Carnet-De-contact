f5ba4c42da56316ef29fa6adb501ba9d7c87881e
betsaleel yoto <betsaleelyoto@gmail.com>
2023-12-01 14:25:38 +0200

    version presque prete

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // script.js
+
 let contacts = [];
 const submit = document.querySelector("#form_button_submit");
+const PhotoDeProfil = document.querySelector("#form_input_photo");
+
+function getImage() {
+  console.log("images", this.files[0]);
+  const imageToProcess = this.files[0];
+  let newImage = new Image(imageToProcess.width, imageToProcess.height);
+  newImage.src = URL.createObjectURL(imageToProcess);
+
+  const spanImage = document.createElement("span");
+  spanImage.classList.add("span_images");
+  spanImage.appendChild(newImage);
+
+  const contactList = document.querySelector(".ma_liste");
+  const li = document.createElement("li");
+  li.appendChild(spanImage);
+
+  contactList.appendChild(li);
+}
+
 function addContact() {
   const firstName = document.querySelector(".prenom").value;
   const lastName = document.querySelector(".nom").value;
@@ -8,8 +28,6 @@ function addContact() {
   const group = document.querySelector(".groupe").value;
   const email = document.querySelector(".email").value;
   const bio = document.querySelector(".bio").value;
-
-  // Validation logic can be added here
 
   const contact = {
     firstName,
@@ -26,50 +44,35 @@ function addContact() {
 }
 
 function displayContacts() {
-  // const profilePic = document.querySelector("#profilePic");
-  // const inputFile = document.querySelector("#form_input_photo");
-
-  // let file = input.files[0];
-
-  // if (file) {
-  //   var reader = new FileReader();
-
-  //   reader.onload = function (e) {
-  //     photoProfil.src = e.target.result;
-  //   };
-
-  //   reader.readAsDataURL(file);
-  // }
-
   const contactList = document.querySelector(".ma_liste");
   contactList.innerHTML = "";
 
   contacts.forEach((contact, index) => {
     const li = document.createElement("li");
     li.innerHTML = `
-    <span class="span_images"> 
-    <img src="#" alt="photo de profil" id= "profilePic">
-</span>
-<div class="contacts_details_nom_tel_groupe_paragraphe">
-    <div class="nom_et_groupe_tel">
-        <span class="prenom_nom_groupe">
-            <p>${contact.firstName} ${contact.lastName} - ${contact.group}   <button onclick="editContact(${index})">modifier</button>  <button onclick="deleteContact(${index})">supprimer</button></p>
-        </span><br>
-        <span class="telephone">
+      <span class="span_images" id="profilePic"></span>
+      <div class="contacts_details_nom_tel_groupe_paragraphe">
+        <div class="nom_et_groupe_tel">
+          <span class="prenom_nom_groupe">
+            <p>${contact.firstName} ${contact.lastName} - ${contact.group}
+              <button onclick="editContact(${index})">modifier</button>
+              <button onclick="deleteContact(${index})">supprimer</button>
+            </p>
+          </span><br>
+          <span class="telephone">
             <p>${contact.phoneNumber}</p>
-        </span><br>
-    </div>
-    <div class="paragraphe_bio">
-        <span class="paragraphe_propre">
+          </span><br>
+        </div>
+        <div class="paragraphe_bio">
+          <span class="paragraphe_propre">
             <p>${contact.bio}</p>
-        </span>
-    </div>
-
-
-</div>
-         `;
+          </span>
+        </div>
+      </div>
+    `;
     contactList.appendChild(li);
   });
+  imageShow();
 }
 
 function editContact(index) {
@@ -80,26 +83,37 @@ function editContact(index) {
   document.querySelector(".groupe").value = contact.group;
   document.querySelector(".email").value = contact.email;
   document.querySelector(".bio").value = contact.bio;
-  // document.getElementById("profilePic").value = contact.profilePic;
 
-  // Remove the edited contact from the array
   contacts.splice(index, 1);
 
   displayContacts();
 }
 
 function deleteContact(index) {
-  // Remove the contact from the array
   contacts.splice(index, 1);
 
   displayContacts();
 }
 
 function clearForm() {
-  // Clear form fields
   document.querySelector(".form_style").reset();
 }
 
+function imageShow() {
+  let a = document.querySelector("#profilePic");
+  let b = new FileReader();
+  b.readAsDataURL(PhotoDeProfil.files[0]);
+  b.onload = () => {
+    let c = b.result;
+    a.innerHTML = `<img src="${c}" alt="image">`;
+  };
+}
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  getImage.call(PhotoDeProfil);
+  addContact();
+});
+
 // Initial display
 displayContacts();
-submit.addEventListener("click", addContact);
